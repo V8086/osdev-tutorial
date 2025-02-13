71f47efc711275513d67ba09baff691ea4b8c82a
V8086 <vmode8086@gmail.com>
2025-02-13 12:19:15 +0300

    Fix lang err: RU->EN

--- Part 2/src/bootldr/second-stage/tty.asm
+++ Part 2/src/bootldr/second-stage/tty.asm
@@ -6,9 +6,9 @@ ttyputc:
 	push bx
 	
 	mov bx, sp
-	mov ah, 0x0e	; функция 0x0e (вывод символа)
-	mov al, [bx+8]	; символ (читаем из стека и записываем в al)
-	int 0x10		; 0x10-ый сервис BIOS (tty)
+	mov ah, 0x0e	; function 0x0e (print symbol)
+	mov al, [bx+8]	; symbol (read from the stack and write to al)
+	int 0x10		; 0x10th BIOS service (tty)
 
 	pop bx
 	pop ax
